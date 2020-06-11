0184d8cab55373989aca8c091e85b2965b926a22
4a568c032 <66801696+4a568c032@users.noreply.github.com>
2020-06-11 14:39:59 -0600

    Updated class, added use case
    data+behavior class w/ custom exception

--- montys-color.rb
+++ montys-color.rb
@@ -1,8 +1,41 @@
 # Lesson 3: Prompt for Favorite Color
 
-# puts "What is your favorite color?"
-# color = gets.chop
-# puts "Wow! My favorite color is #{color} also!"
+class FavoriteColor
+
+  attr_reader :favorite
+
+  class YourFavoriteColorSucks < StandardError
+    def initialize(stupid_color)
+       @wrong = stupid_color
+    end
+
+    def exception
+      puts "Your favorite color #{@wrong} is stupid"
+      super()
+    end
+  end
+
+  def initialize(color)
+    @favorite = color
+  end
+
+  def favorite_color_matches?(other_color)
+    raise YourFavoriteColorSucks.new(other_color) unless @favorite == other_color
+    true
+  end
+
+end
+
+biased_color = "blue"
+
+puts "What is your favorite color?"
+c = FavoriteColor.new(gets.chomp)
+
+if c.favorite_color_matches?(biased_color)
+  puts "OMG our favorite colors match lolol"
+end
+
+
 
 # Lesson 4: Using an If Statement
 
